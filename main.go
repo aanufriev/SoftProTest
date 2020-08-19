c5094813fde501e166d70c6b81de0a0a6fc15641
aanufriev <anufriev.artem.mail@gmail.com>
2020-08-19 17:01:31 +0300

    Added database check to checkReady

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/aanufriev/SoftProTest/storage"
 )
 
 const (
 	lineProvider = "http://localhost:8000/api/v1/lines/"
 )
 
-type Handler struct{}
+type Handler struct {
+	storage storage.StorageInterface
+}
 
 func (h Handler) checkReady(w http.ResponseWriter, r *http.Request) {
 	sports := []string{"baseball", "football", "soccer"}
@@ -23,11 +28,26 @@ func (h Handler) checkReady(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	err := h.storage.Ping()
+	if err != nil {
+		// вывести ошибку
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	fmt.Println("service is ready")
 }
 
 func main() {
-	handler := Handler{}
+	storage := &storage.PostgresStorage{}
+	err := storage.Open("user=testuser password=test_password dbname=softpro sslmode=disable")
+	if err != nil {
+		log.Fatal("can't open database connection: ", err)
+	}
+
+	handler := &Handler{
+		storage,
+	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ready", handler.checkReady)
