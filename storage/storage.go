c6fd57daa3afe63294cc0c1e4130a708ae90a0ac
aanufriev <anufriev.artem.mail@gmail.com>
2020-08-25 10:56:52 +0300

    Add method Save to storage and change InitDatabase

--- storage/storage.go
+++ storage/storage.go
@@ -2,7 +2,10 @@ package storage
 
 import (
 	"database/sql"
-	"io/ioutil"
+	"fmt"
+	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 const (
@@ -11,6 +14,8 @@ const (
 
 type StorageInterface interface {
 	Ping() error
+	Save(sport string, value string) error
+	GetLastLine(sport string) (float32, error)
 }
 
 type PostgresStorage struct {
@@ -29,25 +34,63 @@ func (ps *PostgresStorage) Open(dataSourceName string) error {
 		return err
 	}
 
-	ps.initDatabase()
+	logrus.WithFields(logrus.Fields{
+		"database":         postgres,
+		"data source name": dataSourceName,
+	}).Info("Open db connection")
 
 	return nil
 }
 
-func (ps PostgresStorage) initDatabase() error {
-	file, err := ioutil.ReadFile("./storage/init.sql")
-	if err != nil {
-		return err
+func (ps PostgresStorage) InitDatabase(sports []string) error {
+	for _, sport := range sports {
+		_, err := ps.db.Exec(
+			`CREATE TABLE IF NOT EXISTS ` + sport + ` (
+			id SERIAL NOT NULL PRIMARY KEY,
+			line REAL NOT NULL,
+			get_at_time TIMESTAMP NOT NULL
+			);`)
+
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
 	}
 
-	_, err = ps.db.Query(string(file))
-	if err != nil {
-		return err
-	}
+	logrus.WithFields(logrus.Fields{
+		"sports": sports,
+	}).Info("Init database")
+
 	return nil
 }
 
 func (ps PostgresStorage) Ping() error {
 	err := ps.db.Ping()
 	return err
+}
+
+func (ps PostgresStorage) Save(sport string, line string) error {
+	_, err := ps.db.Exec(
+		"INSERT INTO "+sport+" (line, get_at_time) VALUES ($1, $2)",
+		line, time.Now(),
+	)
+
+	logrus.WithFields(logrus.Fields{
+		"sport": sport,
+		"line":  line,
+	}).Info("Save line in database")
+	return err
+}
+
+func (ps PostgresStorage) GetLastLine(sport string) (float32, error) {
+	var line float32
+
+	err := ps.db.QueryRow(
+		"SELECT line FROM " + sport + " ORDER BY get_at_time DESC LIMIT 1",
+	).Scan(&line)
+	if err != nil {
+		return 0, err
+	}
+
+	return line, nil
 }
