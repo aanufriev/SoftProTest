194d76a5af8bc13dfcc1073f1b6a43e31631a367
aanufriev <anufriev.artem.mail@gmail.com>
2020-08-25 10:58:33 +0300

    Added parseLine test

---
+++ workersPool/workers_test.go
@@ -0,0 +1,18 @@
+package workersPool
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseLine(t *testing.T) {
+	sport := "soccer"
+	json := `{"lines":{"SOCCER":"1.667"}}`
+
+	expected := "1.667"
+	res, _ := parseLine([]byte(json), sport)
+
+	if !reflect.DeepEqual(expected, res) {
+		t.Errorf("Test failed. Results not match\nGot:\n%v\nExpected:\n%v", res, expected)
+	}
+}
